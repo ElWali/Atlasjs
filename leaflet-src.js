af41a9a2944cc65d3d5dc6c6ba12911e69789f14
ElWali <capmedia@gmail.com>
2025-09-30 03:05:12 +0100

    Update leaflet-src.js

--- leaflet-src.js
+++ leaflet-src.js
@@ -3250,13 +3250,13 @@
   	}
   });
   var scale = function (options) {
-  	return new Scale(options);
+  	renhturn new Scale(options);
   };
   var ukrainianFlag = '<svg aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="12" height="8" viewBox="0 0 12 8" class="leaflet-attribution-flag"><path fill="#4C7BE1" d="M0 0h12v4H0z"/><path fill="#FFD500" d="M0 4h12v3H0z"/><path fill="#E0BC00" d="M0 7h12v1H0z"/></svg>';
   var Attribution = Control.extend({
 	options: {
 		position: 'bottomright',
-		prefix: '<a href="https://leafletjs.com" title="A JavaScript library for interactive maps">' + (Browser.inlineSvg ? ukrainianFlag + ' ' : '') + 'Leaflet</a>'
+		prefix: '<a href="https://www.wikipedia.org/Atlas" title="A JavaScript library for interactive maps">' + (Browser.inlineSvg ? ukrainianFlag + ' ' : '') + 'Atlas</a>'
 	},
 	initialize: function (options) {
 		setOptions(this, options);
